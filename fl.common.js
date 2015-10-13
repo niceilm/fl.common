413700ce03e5b7093a26907c1223b59428fa5059
flynn.park <flynn.park@daumkakao.com>
2015-10-13 21:31:38 +0900

    init files

---
+++ fl.common.js
@@ -0,0 +1,222 @@
+angular.module('fl.common', ['ngMaterial', 'ui.router', 'fl.lazy'])
+  .directive('gridResizer', ['$window', '$timeout', function($window, $timeout) {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attr) {
+        function resize() {
+          var offset = parseInt(attr.gridResizer, 10) || 0;
+          element.height(angular.element(document.body).height() - element.offset().top - offset);
+        }
+
+        angular.element($window).on('resize', resize);
+        scope.$on('$destroy', function() {
+          angular.element($window).off('resize', resize);
+        });
+        $timeout(resize, 0);
+      }
+    }
+  }])
+  .directive('naLoadingComplete', [function() {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attr) {
+        if(attr.loadingComplete == 'hide') {
+          element.hide();
+        } else {
+          element.show();
+        }
+      }
+    }
+  }])
+  .directive('naStopDefault', [function() {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attr) {
+        var eventType = attr.stopDefault || 'click';
+        element.bind(eventType, function(event) {
+          event.preventDefault();
+        });
+      }
+    }
+  }])
+  .directive('naFocus', ['$timeout', function($timeout) {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attr) {
+        var el = element.get(0);
+        try {
+          if(el && typeof el.focus == "function") {
+            $timeout(function() {
+              el.blur();
+              $timeout(function() {
+                el.focus();
+              });
+            });
+          }
+        } catch(me) {
+        }
+      }
+    }
+  }])
+  .directive('naWhenActive', ['$location', function($location) {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        if($location.path() === attrs.naWhenActive) {
+          element.addClass("active");
+        } else {
+
+          element.removeClass("active");
+        }
+      }
+    }
+  }])
+  .directive('naRedirect', function($window, $location) {
+    return {
+      restrict: 'A',
+      link: function postLink(scope, element, attrs) {
+        if(attrs.naRedirect) {
+          var currentUrl = $location.url();
+          $window.location.href = attrs.naRedirect + currentUrl;
+        }
+      }
+    };
+  })
+  .directive('naLinkByLocation', ['$window', function($window) {
+    return {
+      restrict: 'A',
+      link: function postLink(scope, element, attrs) {
+        //  Mobile Safari in standalone mode
+        if(("standalone" in $window.navigator) && $window.navigator.standalone) {
+          element.click(function(event) {
+            event.preventDefault();
+            $window.location.href = attrs.href;
+          });
+        }
+      }
+    };
+  }])
+  .directive('naKeydownThanBlur', [function() {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attr) {
+        element.keydown(function($event) {
+          if($event.keyCode !== 13) {
+            return;
+          }
+          $event.preventDefault();
+          $event.stopPropagation();
+          $event.target.blur();
+        });
+      }
+    }
+  }])
+  .factory('ModalService', ['$mdDialog', '$mdToast', '$lazyLoadHelper', function($mdDialog, $mdToast, $lazyLoadHelper) {
+    return {
+      alert: alert,
+      confirm: confirm,
+      show: show,
+      toast: toast
+
+    };
+    function alert(message, options) {
+      options = options || {};
+      return $mdDialog.show($mdDialog.alert()
+        .title('확인')
+        .content(message)
+        .ariaLabel(options.okLabel || '확인')
+        .ok(options.okLabel || '확인')
+        .targetEvent(options.$event));
+    }
+
+    function confirm(message, options) {
+      options = options || {};
+      return $mdDialog.show($mdDialog.confirm()
+        .title('확인')
+        .content(message)
+        .ariaLabel(options.okLabel || '저장')
+        .ok(options.okLabel || '저장')
+        .cancel(options.cancelLabel || '취소')
+        .targetEvent(options.$event));
+    }
+
+    function show(option) {
+      return $mdDialog.show($lazyLoadHelper.makeBundle(option));
+    }
+
+    function toast(message) {
+      return $mdToast.show($mdToast.simple()
+        .content(message)
+        .position("top right")
+        .hideDelay(3000));
+    }
+  }])
+  .factory('StateManager', ['$rootScope', '$state', function($rootScope, $state) {
+    return {
+      backState: backState
+    };
+    function backState(stateName, params) {
+      $state.go($rootScope.previousState.name || stateName, angular.extend($rootScope.previousState.params, params));
+    }
+  }])
+  .factory('AgentManager', [function() {
+    return {
+      isAndroid: isAndroid,
+      isIOS: isIOS,
+      isMobile: isMobile
+    };
+
+    function isAndroid() {
+      return (new UAParser()).getResult().os.name === "Android";
+    }
+
+    function isIOS() {
+      return (new UAParser()).getResult().os.name === "iOS";
+    }
+
+    function isMobile() {
+      return (new UAParser()).getResult().device.type === "mobile";
+    }
+  }])
+  .factory('LoadingIndicator', ['$timeout', '$rootScope', function($timeout, $rootScope) {
+    var timerPromise = null;
+    $rootScope.isLoading = false;
+    function start() {
+      $timeout.cancel(timerPromise);
+      $rootScope.isLoading = true;
+      timerPromise = $timeout(function() {
+        stop();
+      }, 10000);
+    }
+
+    function stop() {
+      $timeout.cancel(timerPromise);
+      $rootScope.isLoading = false;
+    }
+
+    function isLoading() {
+      return $rootScope.isLoading;
+    }
+
+    return {
+      start: start,
+      stop: stop,
+      isLoading: isLoading
+    };
+  }])
+  .filter('encodeURIComponent', [function() {
+    return encodeURIComponent;
+  }])
+  .run(run);
+
+run.$inject = ['$rootScope'];
+
+function run($rootScope) {
+  $rootScope.$on('$stateChangeSuccess', function(ev, to, toParams, from, fromParams) {
+    $rootScope.previousState = {
+      name: from.name,
+      params: fromParams
+    };
+  });
+
+}
---
+++ spec/fl.commonSpec.js
@@ -0,0 +1,8 @@
+describe('fl.common', function() {
+  beforeEach(module('fl.common', function() {
+  }));
+
+  it('test', function() {
+    expect(true).toBe(true);
+  });
+});
